d418479947077861f54cef41cf941fe21839840e
sushantpraveen <103406456+sushantpraveen@users.noreply.github.com>
2023-01-11 02:12:59 +0530

    Add files via upload

---
+++ Matching game/matching game.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+class ctet{
+    public:
+    int marks;
+    string name;
+};
+int main()
+{
+    ctet pass;
+    pass.marks=89;
+    pass.marks=75;
+    pass.marks=70;
+    pass.name="micky";
+    pass.name="sannu";
+    pass.name="bhavi";
+    cout<<pass.marks<<pass.marks<<endl;
+    return 0;
+
+}
